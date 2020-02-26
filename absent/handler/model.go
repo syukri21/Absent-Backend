df23c2a23a3f224bb3a45d254e47046313c27a02
myari211 <syukrihsb148@gmail.com>
2020-02-27 03:35:29 +0700

    fix add model.go

--- absent/handler/model.go
+++ absent/handler/model.go
@@ -16,8 +16,8 @@ type Absent struct {
 	StudentID  uint            `json:"studentId"`
 	TeacherID  uint            `json:"teacherId"`
 	CourseID   uint            `json:"couresId"`
-	AbsentTime *time.Time      `json:"absentTome"`
+	AbsentTime *time.Time      `json:"absentTime"`
 	Student    student.Student `gorm:"foreignkey:StudentID;association_foreignkey:UserID"`
 	Teacher    teacher.Teacher `gorm:"foreignkey:TeacherID;association_foreignkey:UserID"`
-	Course     course.Courese
+	Course     course.Course
 }
--- migrate.go
+++ migrate.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	absent "backend-qrcode/absent/handler"
+	course "backend-qrcode/course/handler"
 	student "backend-qrcode/student/handler"
 	teacher "backend-qrcode/teacher/handler"
 	user "backend-qrcode/user/handler"
+
 	"log"
 
 	"github.com/jinzhu/gorm"
@@ -12,14 +15,17 @@ import (
 // Migrate ...
 func Migrate(db *gorm.DB) {
 
-	err := db.AutoMigrate(&user.User{}, &teacher.Teacher{}, &student.Student{}).Error
+	err := db.AutoMigrate(&user.User{}, &teacher.Teacher{}, &student.Student{}, &course.Course{}, &absent.Absent{}).Error
 
 	if err != nil {
 		log.Fatal("Error Migration", err)
 	}
 
 	db.Model(&teacher.Teacher{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
+	db.Model(&teacher.Teacher{}).AddForeignKey("user_id", "absents(teacher_id)", "CASCADE", "CASCADE")
+
 	db.Model(&student.Student{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
+	db.Model(&student.Student{}).AddForeignKey("user_id", "absents(student_id)", "CASCADE", "CASCADE")
 
 	fullname := "Syukri Husaibatul Khairi"
 	nid := "1234567890"
