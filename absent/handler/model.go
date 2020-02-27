be41d70c9ff04b2bef87bcb5e8b5506bded603fa
myari211 <syukrihsb148@gmail.com>
2020-02-28 02:03:13 +0700

    add jwt to absent

--- absent/handler/model.go
+++ absent/handler/model.go
@@ -4,15 +4,18 @@ import (
 	course "backend-qrcode/course/handler"
 	student "backend-qrcode/student/handler"
 	teacher "backend-qrcode/teacher/handler"
+	"os"
 
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/jinzhu/gorm"
 )
 
 // Absent ...
 type Absent struct {
 	gorm.Model
+	AbsentHash string          `json:"-" gorm:"unique_index"`
 	StudentID  uint            `json:"studentId"`
 	TeacherID  uint            `json:"teacherId"`
 	CourseID   uint            `json:"couresId"`
@@ -20,4 +23,21 @@ type Absent struct {
 	Student    student.Student `gorm:"foreignkey:StudentID;association_foreignkey:UserID"`
 	Teacher    teacher.Teacher `gorm:"foreignkey:TeacherID;association_foreignkey:UserID"`
 	Course     course.Course
+}
+
+// JWTToken ...
+type JWTToken struct {
+	Token string `json:"token"`
+}
+
+// GenerateJWT ...
+func (a Absent) GenerateJWT() (JWTToken, error) {
+	signingKey := []byte(os.Getenv("JWT_ABSENSI_SECRET"))
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"exp":         time.Now().Add(time.Hour * 1 * 1).Unix(),
+		"teacher_id":  int(a.TeacherID),
+		"absent_hash": "aa",
+	})
+	tokenString, err := token.SignedString(signingKey)
+	return JWTToken{tokenString}, err
 }
